3748669fc9fc236dbd84705aecc496d738ba5523
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2021-03-03 18:33:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,13 +41,14 @@ def clnt():
     client.start()
     client.run_until_disconnected()
 
-threading.Thread(target=clnt).start()
-updater = Updater(TOKEN)
-updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
-dp = updater.dispatcher
-dp.add_handler(CommandHandler('start',start))
-updater.bot.setWebhook('https://metgaccountmsgr.herokuapp.com/' + TOKEN)
-updater.idle()
+clnt()
+# threading.Thread(target=clnt).start()
+# updater = Updater(TOKEN)
+# updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
+# dp = updater.dispatcher
+# dp.add_handler(CommandHandler('start',start))
+# updater.bot.setWebhook('https://metgaccountmsgr.herokuapp.com/' + TOKEN)
+# updater.idle()
 
 
        
